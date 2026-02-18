2188cadf6794efeac2d2836048c1ce017aad9071
willyO-o <marcggmarc.20@gmail.com>
2026-02-18 19:10:02 -0400

    subiendo mandamientos eliminar

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -14,5 +14,30 @@ axiosInstance.interceptors.request.use((config) => {
 });
 
 
+// Add a response interceptor to handle 401 errors y renuew the token if necessary
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            try {
+                const refreshToken = localStorage.getItem("refreshToken");
+                const response = await axios.post(`${import.meta.env.VITE_API_URL}/auth/refresh`, { refreshToken });
+                const { token } = response.data;
+                localStorage.setItem("token", token);
+                originalRequest.headers.Authorization = `Bearer ${token}`;
+                return axiosInstance(originalRequest);
+            } catch (err) {
+                console.error("Error refreshing token:", err);
+                // Optionally, you can log the user out or redirect to the login page
+                localStorage.removeItem("token");
+                window.location.href = "/login";
+            }
+        }
+        return Promise.reject(error);
+    }
+);
+
 
 
